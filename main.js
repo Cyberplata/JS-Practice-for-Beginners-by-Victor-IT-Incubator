c1cd6528781781df30a1546f85a6416b6bf9befe
Platon <kininplaton@gmail.com>
2023-03-24 21:46:38 +0300

    Условные конструкции

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 //переменные
-const name = 'Bob';
+const userName = 'Bob';
 let age = 23;
+//условные конструкции
+if(userName === 'Bob'){
+    alert(`Привет, ${userName}!`);
+} else {
+    alert(`Я тебя не знаю`)
+}
